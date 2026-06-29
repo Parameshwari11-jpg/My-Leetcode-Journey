630869b5f605a48c9997831ea336a29761d97799
PARAMESHWARI <paramesh.ravi11@gmail.com>
2026-06-29 10:43:32 +0530

    Time: 87 ms (68.25%), Space: 46.6 MB (52.38%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        boolean[] prime=new boolean[right+1];
+        Arrays.fill(prime,true);
+        if(right>=0) prime[0]=false;
+        if(right>=1) prime[1]=false;
+        for(int i=2;i*i<=right;i++)
+        {
+            if(prime[i])
+            {
+                for(int j=i*i;j<=right;j+=i)
+                {
+                    prime[j]=false;
+                }
+            }
+        }
+        int prev=-1;
+        int minDiff=Integer.MAX_VALUE;
+        int[] ans={-1,-1};
+        for(int i=left;i<=right;i++)
+        {
+            if(prime[i])
+            {
+                if(prev!=-1 && i-prev<minDiff)
+                {
+                    minDiff=i-prev;
+                    ans[0]=prev;
+                    ans[1]=i;
+                }
+                prev=i;
+            }
+        }
+        return ans;
+    }
+}
